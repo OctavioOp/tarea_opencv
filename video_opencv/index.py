011c3ebc8a7ae002e612b26c858d9cb0877e4229
OctavioOp <oc.opazoutlook.com>
2021-06-11 20:39:37 -0400

    Se arregla captura de video

--- video_opencv/index.py
+++ video_opencv/index.py
@@ -24,7 +24,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the resulting frame
-    cv2.inshow('Video',frame)
+    cv2.imshow('Video',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
